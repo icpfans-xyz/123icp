d508e032884b7db10d47c43e83efa9a39265b8e6
纪庆键 <jqjjian@126.com>
2021-06-18 11:14:57 +0800

    add ic.rocks

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-05-26 01:21:00
- * @LastEditTime: 2021-06-11 11:44:56
+ * @LastEditTime: 2021-06-18 11:14:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /oms-ux/Users/chenglei/work/WebStack-vue/src/assets/data.js
@@ -56,6 +56,12 @@ export default [
                 logo: 'assets/images/logos/Dfinity Explorer.jpg',
                 title: 'Dfinity Explorer',
                 desc: 'dfinityexplorer.org'
+            },
+            {
+                url: 'https://ic.rocks/',
+                logo: 'assets/images/logos/icrocks.png',
+                title: 'ic.rocks',
+                desc: 'ic.rocks'
             },
         ]
     },
